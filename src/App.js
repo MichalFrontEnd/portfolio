13c9491ce12a61b0ab2843182e5b79263a96b857
Michal Front <michal.front@outlook.com>
2021-04-14 00:28:55 +0200

    Initial commit and then some

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, BrowserRouter, Route } from "react-router-dom";
+import Contact from "./Contact"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1>Michal Front</h1>
+          <h3>Full-Stack Web Developer</h3>
+          <Contact />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
