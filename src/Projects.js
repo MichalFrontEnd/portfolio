60e1af75d76c6dceaef39f1327cdd9dd6fb374cd
Michal Front <michal.front@outlook.com>
2021-04-21 21:58:17 +0200

    Try to import classes from arr

--- src/Projects.js
+++ src/Projects.js
@@ -2,22 +2,16 @@ import React, {Fragment, useEffect, useState} from 'react';
 import Link from 'react-router';
 import {projectsarr} from './data'
 import classNames from 'classnames'
+import GifPlayer from 'react-gif-player'
 
 export default function Projects() {
     const [projects, setProjects] = useState([])
-    const [classes, setClasses] = useState("")
-
 
     useEffect(()=> {
         setProjects(projectsarr);
-        
-        for (let i=0; i >projects.length; i++) {
-            setClasses(projects[i].class)}
-         
+
     },[projects]);
 
-        console.log('projects: ', projects);
-        console.log('classes: ', classes);
     return (
    
     <Fragment>
@@ -28,6 +22,8 @@ export default function Projects() {
                     <div key={i} className="proj-bg">
                         <div className={proj.class}>
                             <h2>{proj.name}</h2>
+                            {/*{proj.gif && 
+                            (<img src={proj.gif}  alt={`GIF of ${proj.name}`}></img> )}*/}
                             <h4>{proj.liner}</h4>
                         </div>
                     </div>
