5041e8d45be6ec378c194ec3727f5f831cd8b815
Michal Front <michal.front@outlook.com>
2021-04-26 20:03:31 +0200

    Complete Gif section

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,12 @@
 import React, {Fragment, useEffect, useState} from 'react';
-import Link from 'react-router';
+import {Link} from 'react-router-dom';
 import {projectsarr} from './data'
 import classNames from 'classnames'
 import GifPlayer from 'react-gif-player'
 
 export default function Projects() {
     const [projects, setProjects] = useState([])
+    const [isPlaying, setIsPlaying] = useState(false)
 
     useEffect(()=> {
         setProjects(projectsarr);
@@ -19,19 +20,20 @@ export default function Projects() {
         {projects && 
             projects.map((proj, i)=> {
                 return (
-                    <div key={i} className="proj-bg">
+                    <Link to= {`/${proj.folder}`} key={i} className="proj-bg">
                         <div className={proj.class}>
                             <h2>{proj.name}</h2>
                             {proj.gif ? 
                                 <GifPlayer 
                                     gif={proj.gif}
                                     still={`${proj.imgsrc}/1.png`}
-                                    //pauseRef={pause => this.pauseGif = pause}
+                                    onTogglePlay={(e)=> {setIsPlaying(!isPlaying)}}
+
                                 /> : <img src="images\Comingsoon.png" alt="Coming Soon"></img>
                             }
                             <h4>{proj.liner}</h4>
                         </div>
-                    </div>
+                    </Link>
                 );
             })}
     </Fragment>
