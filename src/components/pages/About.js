f3d8aa38b9b68f8e308d3030a76efec2ab9d2667
Michal Front <michal.front@outlook.com>
2021-06-07 19:51:34 +0200

    Complete About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Button } from 'reactstrap';
+import { Container,} from 'reactstrap';
 
 import img from '../assets/img/about.jpg'
 export default function about() {
@@ -11,8 +11,14 @@ export default function about() {
                     <img src={img} alt='mfront'/>
                     <div className="about-text">
                         <h2>Hi! I'm Michal Front</h2>
-                        <h5>I'm a full stack web developer from Berlin, Germany.</h5>
-                        <p></p>
+                        <h4>A full stack web developer from Berlin, Germany.</h4>
+                        <p>I have a strong affinity to the Front-End side of things, though I do enjoy getting my hands dirty on some server work.
+I currently work in JavaScript, focusing mainly on React.
+While always striving to learn and implement new things, I also love to dive deeper into the nitty-gritty of my current stack.</p>
+                        
+                        <p>In my spare time I enjoy classical singing, swing dancing, sewing and embroidery.</p>
+                        <p>Available for work.</p>
+
                     </div>
                 </div>
             </Container>
