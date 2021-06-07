5fa71c6b35e7bac13496d5c7a048b67d2f56e298
Michal Front <michal.front@outlook.com>
2021-06-07 12:41:54 +0200

    Progress on About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Jumbotron, Container, Button } from 'reactstrap';
 
-
-export default function Home() {
+import img from '../assets/img/about.jpg'
+export default function about() {
 
     return (
         <div>
             <Container fluid className="gen-container">
-                {/*<h2>Michal Front</h2>
-                <h3>Full-Stack Web Developer</h3>*/}
+                <div className="about-container">
+                    <img src={img} alt='mfront'/>
+                    <div className="about-text">
+                        <h2>Hi! I'm Michal Front</h2>
+                        <h5>I'm a full stack web developer from Berlin, Germany.</h5>
+                        <p></p>
+                    </div>
+                </div>
             </Container>
         </div>
     )
