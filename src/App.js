96ee2ece374fd9fffa9be105e8e333f450089c31
Michal Front <michal.front@outlook.com>
2021-06-07 22:59:22 +0200

    Start rewriting navbar without bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Link, HashRouter, Route, BrowserRouter } from "react-router-dom";
 import Contactbar from "./components/Contactbar"
 import Projects from "./components/pages/Projects"
-import Navibar from "./components/Navbar"
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
@@ -10,16 +9,12 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Navibar>
-      </Navibar>
+
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/projects" component={Projects}></Route>
-        <Route exact path="/about" component={About}></Route>
+      <Route exact path="/about" component={About}></Route>
 
-        {/*<Header />*/}
-        {/*<div className="project-container">
-          <Projects />
-        </div>*/}
+
       </div>
       <Contactbar />
     </BrowserRouter>
