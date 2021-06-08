81d77a8975bc1e9a8adc0c162425af7b138e8ceb
Michal Front <michal.front@outlook.com>
2021-06-08 11:14:56 +0200

    Navbar senza Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
 import './App.css';
-import { Link, HashRouter, Route, Switch, NavLink, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import Contactbar from "./components/Contactbar"
+import Navbar from "./components/Navbar"
 
 //pages
 import Projects from "./components/pages/Projects"
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
-const routes = [
-  {
-    path: "/",
-    exact: true,
-    component: 'Home',
-    navbar: () => <div>Home</div>
-  },
-  {
-    path: "/projects",
-    exact: true,
-    component: 'Projects',
-    navbar: () => <div>Projects</div>
-  },
-  {
-    path: "/about",
-    exact: true,
-    component: 'About',
-    navbar: () => <div>About</div>
-  },
-]
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <navbar>
-          {routes.map((route, i) => (
-            <NavLink
-              key={i}
-              to={route.path}
-              exact={route.exact}
-              className='navlink'
-            >{route.component}</NavLink>
-          ))}
-        </navbar>
+        <Navbar />
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/projects" component={Projects}></Route>
         <Route exact path="/about" component={About}></Route>
-
         </div>
         <Contactbar />
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,41 @@
-import React, {useState} from 'react'
-import { Navbar, Nav, NavLink, Row, Col, activeKey, eventKey} from 'reactstrap';
-import classnames from 'classnames';
+import React from 'react'
+import { NavLink} from "react-router-dom";
 
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: 'Home',
+    navbar: () => <div>Home</div>
+  },
+  {
+    path: "/projects",
+    exact: true,
+    component: 'Projects',
+    navbar: () => <div>Projects</div>
+  },
+  {
+    path: "/about",
+    exact: true,
+    component: 'About',
+    navbar: () => <div>About</div>
+  },
+]
 
 export default function Navibar() {
-    const [active, setActive] = useState(false);
-    console.log('active: ', active);
-//  const [activeTab, setActiveTab] = useState('1');
 
-//  const toggle = tab => {
-//    if(activeTab !== tab) setActiveTab(tab);
-//  }
 
     return (
-            <Row>
-                <Col sm={{offset:8}}>
-                    <Navbar>
-                        <Nav tabs onClick={(k) => setActive(true)} >
-                            <NavLink active id="navlink" href="/">Home</NavLink>
-                            <NavLink active={active} href="/projects"  id="navlink">Projects</NavLink>
-                            <NavLink active={active} href="/about" id="navlink">About</NavLink>
-                        </Nav>
-                    </Navbar>
-                </Col>
-            </Row>
+        <navbar>
+          {routes.map((route, i) => (
+            <NavLink
+              key={i}
+              to={route.path}
+              exact={route.exact}
+              className='navlink'
+            >{route.component}</NavLink>
+          ))}
+        </navbar>
     )
 }
